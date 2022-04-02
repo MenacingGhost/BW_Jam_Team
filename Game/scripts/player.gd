67b20771bd742b488cb798e2c6266facf90ce8b5
mucc zucc bug offcial photoshopper <100082402+mucczuccbugphotoshopper@users.noreply.github.com>
2022-04-02 10:58:56 +0530

    Add files via upload

--- Game/scripts/player.gd
+++ Game/scripts/player.gd
@@ -21,38 +21,36 @@ onready var ground_ray = $ground_ray
 
 var recorded_data = [] # this is our array we update when moving, check when recording
 var is_rewinding = false # we'll use this to disable this object when true. so we can add recorded data to it
-var rewind_length = (60 * 3) # 180 this is 3seconds running at 60fps
+var rewind_length = (60 * 500) # 180 this is 3seconds running at 60fps
 var rewind_ghost = load("res://objects/rewind_ghost.tscn") # direct to a sprite we'll use as a ghost
+var do_rewind = false
 
 func handle_rewind_function():
 	var ani_number = ani.get_index()
 	var dir_number = 0
-	
-	
-	
 
-	is_rewinding = false
-	if(recorded_data.size() > 0):
-		var current_frame = recorded_data[0]
+	if(do_rewind): # DO REWIND
+		is_rewinding = true
+		if(recorded_data.size() > 0):
+			var current_frame = recorded_data[0]
 			
-			
-		if(current_frame != null):
-			ani.animation = current_frame[0]
-			global_position = current_frame[1] 
-			ani.flip_h = current_frame[2]
+			#Set our values to the first frame of the array
+			if(current_frame != null):
+				ani.animation = current_frame[0]
+				global_position = current_frame[1]
+				ani.flip_h = current_frame[2]
 				
-			var ghost : Sprite = rewind_ghost.instance()
-			ghost.texture = ani.frames.get_frame(ani.animation,ani.frame)
-			ghost.global_position = global_position
-			ghost.flip_h = ani.flip_h
-			get_parent().add_child(ghost)
+				var ghost : Sprite = rewind_ghost.instance()
+				ghost.texture = ani.frames.get_frame(ani.animation,ani.frame)
+				ghost.global_position = global_position
+				ghost.flip_h = ani.flip_h
+				get_parent().add_child(ghost)
 				
-			
+			#remove thet first frame as we've just used it
 			recorded_data.pop_front()
 			
 	else: # WE are recording
-		is_rewinding = true
-		$Timer.start()
+		is_rewinding = false
 		
 		if(ani.flip_h):
 			dir_number = 1
@@ -60,10 +58,15 @@ func handle_rewind_function():
 			dir_number = 0
 		
 		recorded_data.push_front([ani.animation,global_position,ani.flip_h])
-		if(recorded_data.size() > rewind_length): 
+		if(recorded_data.size() > rewind_length): #our record is longer than 3 secs, remove last frame
 			recorded_data.pop_back()
 
+			
+		
+			
+
 func _physics_process(delta):
+	handle_rewind_function()
 	if(is_rewinding):
 		Engine.time_scale = 0.5
 	if(!is_rewinding):
@@ -90,6 +93,9 @@ func do_physics(delta):
 	#update our motion vector
 	motion.y = vSpeed
 	motion.x = hSpeed
+	
+	
+		
 	
 	#apply our motion vectgor to move and slide
 	motion = move_and_slide(motion,UP)
@@ -145,4 +151,9 @@ func handle_movement(var delta):
 
 
 func _on_Timer_timeout():
-	handle_rewind_function()
+	do_rewind = true
+
+	
+
+func _on_Norewind_body_entered(body):
+	do_rewind = false
